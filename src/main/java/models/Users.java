ef2051631d5fdd90b73102fa2990ce21b20ac8a5
CatalinR <cristescu.razvan@outlook.com>
2024-02-25 22:08:57 +0200

    reverted from local history

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,2 +1,25 @@
-package models;public class Users {
+package models;
+
+import java.util.ArrayList;
+
+ class Data{
+    public int id;
+    public String email;
+    public String first_name;
+    public String last_name;
+    public String avatar;
+}
+
+public class Users{
+    public int page;
+    public int per_page;
+    public int total;
+    public int total_pages;
+    public ArrayList<Data> data;
+    public Support support;
+}
+
+  class Support{
+    public String url;
+    public String text;
 }
--- src/test/java/api/RestTests.java
+++ src/test/java/api/RestTests.java
@@ -2,29 +2,66 @@ package api;
 
 import io.qameta.allure.*;
 import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.qameta.allure.SeverityLevel.CRITICAL;
+import static io.qameta.allure.SeverityLevel.NORMAL;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
 
 public class RestTests {
 
 
+    @BeforeAll
+    public static void beforeAllSetup(){
+        RestAssured.baseURI = "https://reqres.in/api";
+    }
     @Test
-    @DisplayName("Test Authentication")
-    @Description("This test attempts to log into the website using a login and a password. Fails if any error happens.\n\nNote that this test does not test 2-Factor Authentication.")
-    @Severity(CRITICAL)
-    @Owner("John Doe")
-    @Link(name = "Website", url = "https://dev.example.com/")
-    @Issue("AUTH-123")
-    public void GetRequest()
+    @DisplayName("GET users - json validator org.hamcrest.MatcherAssert")
+    @Description("This test attempts to call the /users and validate against the json schema using the org.hamcrest.MatcherAssert")
+    @Severity(NORMAL)
+    @Owner("Razvan")
+    @Link(name = "Regres.in", url = "https://reqres.in/api")
+    @Issue("Jira-01")
+    public void getUsersValidateJsonHamcrestWay() {
 
-    {
+        var response =  given()
+               // .log().all() // Log request details
+                .when()
+                .get("/users") // When: Sending the GET request to /users endpoint
+                .then()
+               // .log().all() // Log response details
+                .statusCode(200) // Then: Verify response status code is 200 OK
+                .body("data", not(empty()));
 
-        RestAssured.baseURI = "https://dummy.restapiexample.com/";
-       var response =  given().when().get("api/v1/employees").then().extract().response();
 
-        System.out.println(response.asString());
+        MatcherAssert.assertThat(
+                "Validate json schema",
+                response.extract().response().asString(),
+                JsonSchemaValidator.matchesJsonSchemaInClasspath("usersSchema.json")
+        );
+
+    }
+
+    @Test
+    @DisplayName("GET users - json validator io.restassured.module.jsv.JsonSchemaValidator")
+    @Description("This test attempts to call the /users and validate against the json schema using the io.restassured.module.jsv.JsonSchemaValidator")
+    @Severity(NORMAL)
+    @Owner("Razvan")
+    @Link(name = "Regres.in", url = "https://reqres.in/api")
+    @Issue("Jira-02")
+    public void GetUsersValidateJsonRestAssuredWay() {
+
+                 given()
+                .when()
+                    .get("/users")
+                .then()
+                    .statusCode(200)
+                    .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("usersSchema.json"));
+
     }
 }
